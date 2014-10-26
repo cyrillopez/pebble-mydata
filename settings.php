2912e52c1fbbdeabec8341023eb809e28a38c960
cyrillopez <cyril.lopez@gmail.com>
2014-10-26 16:43:45 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+# See index for more informations
 
 # Set parameters by default
 
@@ -33,37 +34,50 @@ $setting['DoNotDisturb']['blink'] = 0; // Blink off
 # Settings contents to display
 
 // For each button, you can set content and refresh/blink/vibrate options
+// Content setting can be set to message text or url with content json result
+// Type setting should be set according to content type (text or url)
 
-$setting['default']['content'] = 'Hello World !';
+$setting['default']['content'] = 'Hello World !'; // You can set an url with content json result (then change type to url)
+$setting['default']['type'] = 'text'; //Set Type = text or url 
+$setting['shake']['content'] = 'Shake ok'; 
+$setting['shake']['type'] = 'text'; //text or url
+$setting['shake']['refresh'] = '';  
+$setting['shake']['blink'] = 0; 
+$setting['shake']['vibrate'] = '';
 $setting['up_short']['content'] = 'Up Short'; 
+$setting['up_short']['type'] = 'text'; //text or url
 $setting['up_short']['refresh'] = 15;  
 $setting['up_short']['blink'] = 0; 
 $setting['up_short']['vibrate'] = ''; 
 $setting['up_long']['content'] = 'Up Long'; 
+$setting['up_long']['type'] = 'text'; //text or url
 $setting['up_long']['refresh'] = 5; 
 $setting['up_long']['blink'] = 0; 
 $setting['up_long']['vibrate'] = ''; 
 $setting['down_short']['content'] = 'Down Short'; 
+$setting['down_short']['type'] = 'text'; //text or url 
 $setting['down_short']['refresh'] = 30; 
 $setting['down_short']['blink'] = 0; 
 $setting['down_short']['vibrate'] = '';  
 $setting['down_long']['content'] = 'Down Long'; 
+$setting['down_long']['type'] = 'text'; //text or url
 $setting['down_long']['refresh'] = 15; 
 $setting['down_long']['blink'] = 0;  
 $setting['down_long']['vibrate'] = ''; 
 $setting['select_short']['content'] = 'Select Short'; 
+$setting['select_short']['type'] = 'text'; //text or url
 $setting['select_short']['refresh'] = '';  
 $setting['select_short']['blink'] = 0; 
 $setting['select_short']['vibrate'] = '';  
 $setting['select_long']['content'] = 'Select Long'; 
+$setting['select_long']['type'] = 'text'; //text or url
 $setting['select_long']['refresh'] = 5; 
 $setting['select_long']['blink'] = 0; 
 $setting['select_long']['vibrate'] = ''; 
 $setting['error']['content'] = 'Error ! Access denied !'; 
+$setting['error']['type'] = 'text'; //text or url
 $setting['error']['refresh'] = 900; 
 $setting['error']['blink'] = 5; 
 $setting['error']['vibrate'] = 3; 
-
-// You can use function read_content_from_url to get content from json url like read_content_from_url(url_json_content) 
 
 ?>
